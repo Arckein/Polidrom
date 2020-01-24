1a8ec0d8fac43f882e26d2d78dcb7506ed819c9b
Altessa <sikachev.o@gmail.com>
2020-01-24 10:35:59 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace polidrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
